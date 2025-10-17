d824a69c9ecaacaa58fde3877474891fad1c176d
Sergio <sdominguez894@users.noreply.github.com>
2025-10-17 14:07:04 +0200

    Translate to Typescript

--- src/core/domain/enums/BranchType.js
+++
@@ -1,7 +0,0 @@
-/**
- * Enumeration for branch types
- */
-export const BranchType = Object.freeze({
-  SINGLE: 'SINGLE',     // Single branch
-  DOUBLE: 'DOUBLE'      // Double branch
-});
---
+++ src/core/domain/enums/BranchType.ts
@@ -0,0 +1,8 @@
+/**
+ * Enumeration for branch types
+ */
+export enum BranchType
+{
+    SINGLE = 'SINGLE',  /** Single branch */
+    DOUBLE = 'DOUBLE'   /** Double branch */
+}
