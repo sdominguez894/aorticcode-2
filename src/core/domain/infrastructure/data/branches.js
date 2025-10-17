5609ea35235048fdffc5953286dcc1f635a891e7
Sergio <sdominguez894@users.noreply.github.com>
2025-10-17 10:08:20 +0200

    Create branches.js data file

---
+++ src/core/domain/infrastructure/data/branches.js
@@ -0,0 +1,24 @@
+// Catálogo de ramas
+export const branches = [
+    { code: 'PLC121000', diameter: 12, length: 100 },
+    { code: 'PLC121200', diameter: 12, length: 120 },
+    { code: 'PLC121400', diameter: 12, length: 140 },
+    { code: 'PLC141000', diameter: 14.5, length: 100 },
+    { code: 'PLC141200', diameter: 14.5, length: 120 },
+    { code: 'PLC141400', diameter: 14.5, length: 140 },
+    { code: 'PLC161000', diameter: 16, length: 95 },
+    { code: 'PLC161200', diameter: 16, length: 115 },
+    { code: 'PLC161400', diameter: 16, length: 135 },
+    { code: 'PLC181000', diameter: 18, length: 95 },
+    { code: 'PLC181200', diameter: 18, length: 115 },
+    { code: 'PLC181400', diameter: 18, length: 135 },
+    { code: 'PLC201000', diameter: 20, length: 95 },
+    { code: 'PLC201200', diameter: 20, length: 115 },
+    { code: 'PLC201400', diameter: 20, length: 135 },
+    { code: 'PLC231000', diameter: 23, length: 100 },
+    { code: 'PLC231200', diameter: 23, length: 120 },
+    { code: 'PLC231400', diameter: 23, length: 140 },
+    { code: 'PLC271000', diameter: 27, length: 100 },
+    { code: 'PLC271200', diameter: 27, length: 120 },
+    { code: 'PLC271400', diameter: 27, length: 140 }
+];
