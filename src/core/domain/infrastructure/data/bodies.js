a62b50deb12199d15b76e972e6ccd2ff0b0ec2d3
Sergio <sdominguez894@users.noreply.github.com>
2025-10-17 10:07:53 +0200

    Create bodies.js

---
+++ src/core/domain/infrastructure/data/bodies.js
@@ -0,0 +1,9 @@
+// Catálogo de prótesis con correcciones de longitud de patas
+export const bodies = [
+    { code: 'CXT201412E', diameter: 20, length: 55, shortLeg: 30, longLeg: 65 },
+    { code: 'CXT231412E', diameter: 23, length: 55, shortLeg: 30, longLeg: 65 },
+    { code: 'CXT261412E', diameter: 26, length: 55, shortLeg: 30, longLeg: 65 },
+    { code: 'CXT281412E', diameter: 28.5, length: 55, shortLeg: 30, longLeg: 65 },
+    { code: 'CXT321414E', diameter: 32, length: 65, shortLeg: 30, longLeg: 75 },
+    { code: 'CXT361414E', diameter: 36, length: 65, shortLeg: 30, longLeg: 75 }
+];
