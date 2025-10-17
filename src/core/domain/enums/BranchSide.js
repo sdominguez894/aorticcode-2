2b2ab7e84d7568a54284bb058da0f1e985fc6304
Sergio <sdominguez894@users.noreply.github.com>
2025-10-17 14:05:38 +0200

    Translate to Typescript

--- src/core/domain/enums/BranchSide.js
+++
@@ -1,7 +0,0 @@
-/**
- * Enumeration for branch sides
- */
-export const BranchSide = Object.freeze({
-    IPSILATERAL: 'IPSILATERAL',      // Same side as main body insertion
-    CONTRALATERAL: 'CONTRALATERAL'   // Opposite side
-});
---
+++ src/core/domain/enums/BranchSide.ts
@@ -0,0 +1,8 @@
+/**
+ * Enumeration for branch sides
+ */
+export enum BranchSide
+{
+    IPSILATERAL = 'IPSILATERAL',        /** Same side as main body insertion */
+    CONTRALATERAL = 'CONTRALATERAL'     /** Opposite side to main body insertion */
+}
