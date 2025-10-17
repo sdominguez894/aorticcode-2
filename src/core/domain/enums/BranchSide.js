7c69688a5a49faa08945924d53b1772a50cefe40
Sergio <sdominguez894@users.noreply.github.com>
2025-10-17 10:02:40 +0200

    Create BranchSide.js

---
+++ src/core/domain/enums/BranchSide.js
@@ -0,0 +1,7 @@
+/**
+ * Enumeration for branch sides
+ */
+export const BranchSide = Object.freeze({
+    IPSILATERAL: 'IPSILATERAL',      // Same side as main body insertion
+    CONTRALATERAL: 'CONTRALATERAL'   // Opposite side
+});
